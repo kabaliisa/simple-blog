eec38ffa0598f8ab6a9eb31f27a304dd03cd034f
kabaliisa <kabaliisa15@gmail.com>
2021-04-24 18:06:03 +0300

    adds components

---
+++ src/pages/AllMeetups.js
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import MeetupList from "../components/layout/meetups/MeetupList";
+
+function AllMeetups() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    fetch(
+      "https://react-getting-started-500a0-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading ...</p>
+      </section>
+    );
+  }
+
+  return (
+    <section>
+      <h1>All meetups</h1>
+      <MeetupList meetups={loadedMeetups} />
+    </section>
+  );
+}
+
+export default AllMeetups;
