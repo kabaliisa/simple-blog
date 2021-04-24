e12700b564cbf4239197bdd44f50f5f2fcf3dd1c
kabaliisa <kabaliisa15@gmail.com>
2021-04-24 18:04:05 +0300

    posts data

---
+++ src/pages/NewMeetup.js
@@ -0,0 +1,31 @@
+import { useHistory } from "react-router-dom";
+
+import NewMeetupForm from "../components/layout/meetups/NewMeetupForm";
+
+function NewMeetup() {
+  const history = useHistory();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-getting-started-500a0-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    ).then(() => {
+      history.replace("/");
+    });
+  }
+
+  return (
+    <section>
+      <h1> Add new meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
+
+export default NewMeetup;
